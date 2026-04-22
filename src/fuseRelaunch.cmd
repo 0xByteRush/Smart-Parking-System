1520c048a336500986ca10a535ccff30ef3aff77
0xByteRush <75476244+0xByteRush@users.noreply.github.com>
2026-04-22 17:19:07 +0300

    Delete src directory

--- src/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "C:/Xilinx/PROSS/Smart_Parking_System_isim_beh.exe" -prj "C:/Xilinx/PROSS/Smart_Parking_System_beh.prj" "work.Smart_Parking_System" "work.glbl" 

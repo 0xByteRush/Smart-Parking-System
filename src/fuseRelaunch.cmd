d5e8c883c9c75391ccbdba96896baf778ac34ccc
0xByteRush <75476244+0xByteRush@users.noreply.github.com>
2026-04-22 17:15:41 +0300

    Add files via upload

---
+++ src/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "C:/Xilinx/PROSS/Smart_Parking_System_isim_beh.exe" -prj "C:/Xilinx/PROSS/Smart_Parking_System_beh.prj" "work.Smart_Parking_System" "work.glbl" 
